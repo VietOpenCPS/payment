a1023f99c8a9dbb22f197a28b8630f98ad55986f
nguyennv <nguyennv1981@gmail.com>
2016-05-27 16:53:39 +0700

    First commit payment exception

---
+++ core/src/main/java/org/opencps/payment/exception/PaymentException.java
@@ -0,0 +1,30 @@
+/**
+* OpenCPS Payment is the open source Core Payment Connector software
+* Copyright (C) 2016-present OpenCPS community
+
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+package org.opencps.payment.exception;
+
+import java.io.IOException;
+
+/**
+ * @author Nguyen Van Nguyen <nguyennv@iwayvietnam.com>
+ */
+public class PaymentException extends IOException {
+	private static final long serialVersionUID = 6039606916479805421L;
+
+	public PaymentException(String string) {
+        super(string);
+	}
+}
