96b748d0ff27763f91be48a3feef8b18690666bd
nguyennv <nguyennv1981@gmail.com>
2016-05-31 17:34:13 +0700

    Add abstract class of PaymentRequest

---
+++ core/src/main/java/org/opencps/payment/RequestBase.java
@@ -0,0 +1,447 @@
+/**
+* OpenCPS Payment is the open source Core Payment Connector software
+* Copyright (C) 2016-present OpenCPS community
+
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+package org.opencps.payment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.collections4.IterableMap;
+import org.apache.commons.collections4.map.HashedMap;
+import org.opencps.payment.api.PaymentItem;
+import org.opencps.payment.api.PaymentRequest;
+import org.opencps.payment.api.PaymentResponse;
+import org.opencps.payment.exception.InvalidRequestException;
+
+/**
+ * This abstract class implements PaymentRequest and defines a basic
+ * set of functions that all OpenCPS Payment Requests are intended to include.
+ * Requests of this class are usually created using the createRequest
+ * method of the connector and then actioned using methods within this
+ * class or a class that extends this class.
+ * 
+ * @author Nguyen Van Nguyen <nguyennv@iwayvietnam.com>
+ */
+public abstract class RequestBase implements PaymentRequest {
+
+    /**
+     * The request parameters
+     */
+    protected IterableMap<String, String> parameters;
+    
+    /**
+     * The HTTP transport object.
+     */
+    protected ConnectorBase connector;
+
+    /**
+     * The item parameters
+     */
+    protected List<PaymentItem> items;
+
+    /**
+     * An associated PaymentResponse.
+     */
+    protected PaymentResponse response;
+    
+    /**
+     * The credit card parameter
+     */
+    protected CreditCard card;
+    
+    protected static Boolean zeroAmountAllowed = true;
+
+    protected static Boolean negativeAmountAllowed = false;
+    
+    public RequestBase(ConnectorBase connector) {
+        this.connector = connector;
+        parameters = new HashedMap<String, String>();
+        items = new ArrayList<PaymentItem>();
+    }
+
+    /**
+     * (non-Javadoc)
+     * @throws InvalidRequestException 
+     * @see org.opencps.payment.api.PaymentRequest#initialize()
+     */
+    @Override
+    public PaymentRequest initialize(IterableMap<String, String> parameters) throws RuntimeException {
+        if (response != null) {
+            throw new RuntimeException("Request cannot be modified after it has been sent!");
+        }
+        if (this.parameters == null) {
+            this.parameters = new HashedMap<String, String>();
+        }
+        for (Map.Entry<String, String> entry: parameters.entrySet()) {
+            this.parameters.put(entry.getKey(), entry.getValue());
+        }
+        return this;
+    }
+    
+    /**
+     * (non-Javadoc)
+     * @see org.opencps.payment.api.PaymentRequest#getParameters()
+     */
+    @Override
+    public IterableMap<String, String> getParameters() {
+        return parameters;
+    }
+    
+    /**
+     * Get a single parameter.
+     */
+    protected String getParameter(String key) {
+        return parameters.get(key);
+    }
+    
+    /**
+     * Set a single parameter
+     */
+    protected RequestBase setParameter(String key, String value) throws RuntimeException {
+        if (response != null) {
+            throw new RuntimeException("Request cannot be modified after it has been sent!");
+        }
+        parameters.put(key, value);
+        return this;
+    }
+    
+    /**
+     * Get test mode of the request
+     */
+    public Boolean getTestMode() {
+        try {
+            return Boolean.valueOf(getParameter("testMode"));
+        }
+        catch (NumberFormatException ex) {
+            return false;
+        }
+    }
+    
+    /**
+     * Set test mode of the request
+     */
+    public RequestBase setTestMode(Boolean value) {
+        return setParameter("testMode", value.toString());
+    }
+    
+    /**
+     * Get the credit card.
+     */
+    public CreditCard getCard() {
+        return card;
+    }
+    
+    /**
+     * Set the credit card.
+     */
+    public RequestBase setCard(CreditCard card) {
+        this.card = card;
+        return this;
+    }
+
+    /**
+     * Set the credit card with parameters.
+     * @param parameters
+     */
+    public RequestBase setCard(IterableMap<String, String> parameters) {
+        this.card = new CreditCard(parameters);
+        return this;
+    }
+    
+    /**
+     * Get the card token.
+     */
+    public String getToken() {
+        return getParameter("token");
+    }
+
+    /**
+     * Set the card token.
+     */
+    public RequestBase setToken(String value) {
+        return setParameter("token", value);
+    }
+
+    /**
+     * Get the card reference.
+     */
+    public String getCardReference() {
+        return getParameter("cardReference");
+    }
+
+    /**
+     * Set the card reference.
+     */
+    public RequestBase setCardReference(String value) {
+        return setParameter("cardReference", value);
+    }
+
+    /**
+     * Validates and returns the formated amount.
+     */
+    public String getAmount() throws InvalidRequestException {
+        String amount = getParameter("amount");
+        if (amount != null) {
+            if (getCurrencyDecimalPlaces() > 0) {
+                if (Helper.isInteger(amount)) {
+                    throw new InvalidRequestException("Please specify amount as a float string, with decimal places (e.g. \'10.00\' to represent $10.00).");
+                }
+            }
+            Float floatAmount = Helper.toFloat(amount);
+            if (!negativeAmountAllowed && floatAmount < 0) {
+                throw new InvalidRequestException("A negative amount is not allowed.");
+            }
+            
+            if (!zeroAmountAllowed && floatAmount == 0.0f) {
+                throw new InvalidRequestException("A zero amount is not allowed.");
+            }
+            
+            Integer decimalCount = Helper.decimalCount(amount);
+            if (decimalCount > getCurrencyDecimalPlaces()) {
+                throw new InvalidRequestException("Amount precision is too high for currency.");
+            }
+        }
+        return amount;
+    }
+    
+    /**
+     * Sets the payment amount.
+     */
+    public RequestBase setAmount(String value) {
+        return setParameter("amount", value);
+    }
+    
+    /**
+     * Sets the payment amount.
+     */
+    public RequestBase setAmount(Float value) {
+        return setParameter("amount", value.toString());
+    }
+    
+    /**
+     * Get the payment amount as an integer.
+     */
+    public Integer getAmountInteger() throws InvalidRequestException {
+        return Math.round(Helper.toFloat(getAmount()) * getCurrencyDecimalFactor());
+    }
+    
+    /**
+     * Get the payment currency code.
+     */
+    public String getCurrency() {
+        return getParameter("currency");
+    }
+
+    /**
+     * Set the payment currency code.
+     */
+    public RequestBase setCurrency(String value) {
+        return setParameter("currency", value.toUpperCase());
+    }
+
+    /**
+     * Get the payment currency number.
+     */
+    public String getCurrencyNumeric() {
+        Currency currency = Currency.find(getCurrency());
+        if (currency != null) {
+            return currency.getNumeric();
+        }
+        return null;
+    }
+
+    /**
+     * Get the number of decimal places in the payment currency.
+     */
+    public Integer getCurrencyDecimalPlaces() {
+        String sCurrency = getCurrency();
+        if (sCurrency != null && sCurrency.length() > 0) {
+            Currency currency = Currency.find(sCurrency);
+            if (currency != null) {
+                return currency.getDecimals();
+            }
+        }
+        return 2;
+    }
+    
+    private Integer getCurrencyDecimalFactor() {
+        return (int) Math.pow(10, getCurrencyDecimalPlaces());
+    }
+
+    /**
+     * Get the request description.
+     */
+    public String getDescription() {
+        return getParameter("description");
+    }
+    
+    /**
+     * Set the request description.
+     */
+    public RequestBase setDescription(String value) {
+        return setParameter("description", value);
+    }
+
+    /**
+     * Get the transaction ID.
+     * 
+     * The transaction ID is the identifier generated by the merchant website.
+     */
+    public String getTransactionId() {
+        return getParameter("transactionId");
+    }
+    
+    /**
+     * Sets the transaction ID.
+     */
+    public RequestBase setTransactionId(String value) {
+        return setParameter("transactionId", value);
+    }
+
+    /**
+     * Get the transaction reference.
+     * 
+     * The transaction reference is the identifier generated by the remote payment gateway.
+     */
+    public String getTransactionReference() {
+        return getParameter("transactionReference");
+    }
+
+    /**
+     * Sets the transaction reference.
+     */
+    public RequestBase setTransactionReference(String value) {
+        return setParameter("transactionReference", value);
+    }
+
+    /**
+     * Get request items
+     */
+    public List<PaymentItem> getItems () {
+        return items;
+    }
+    
+    /**
+     * Set request items
+     */
+    public RequestBase setItems (List<PaymentItem> items) {
+        this.items = items;
+        return this;
+    }
+
+    /**
+     * Get the client IP address.
+     */
+    public String getClientIp() {
+        return getParameter("clientIp");
+    }
+    
+    /**
+     * Set the client IP address.
+     */
+    public RequestBase setClientIp(String value) {
+        return setParameter("clientIp", value);
+    }
+
+    /**
+     * Get the request return URL.
+     */
+    public String getReturnUrl() {
+        return getParameter("returnUrl");
+    }
+    
+    /**
+     * Set the request return URL.
+     */
+    public RequestBase setReturnUrl(String value) {
+        return setParameter("returnUrl", value);
+    }
+
+    /**
+     * Get the request cancel URL.
+     */
+    public String getCancelUrl() {
+        return getParameter("cancelUrl");
+    }
+    
+    /**
+     * Set the request cancel URL.
+     */
+    public RequestBase setCancelUrl(String value) {
+        return setParameter("cancelUrl", value);
+    }
+
+    /**
+     * Get the request notify URL.
+     */
+    public String getNotifyUrl() {
+        return getParameter("notifyUrl");
+    }
+    
+    /**
+     * Set the request notify URL.
+     */
+    public RequestBase setNotifyUrl(String value) {
+        return setParameter("notifyUrl", value);
+    }
+
+    /**
+     * Get the payment issuer.
+     */
+    public String getIssuer() {
+        return getParameter("issuer");
+    }
+    
+    /**
+     * Set the payment issuer.
+     */
+    public RequestBase setIssuer(String value) {
+        return setParameter("issuer", value);
+    }
+
+    /**
+     * Get the payment method.
+     */
+    public String getPaymentMethod() {
+        return getParameter("paymentMethod");
+    }
+    
+    /**
+     * Set the payment method.
+     */
+    public RequestBase setPaymentMethod(String value) {
+        return setParameter("paymentMethod", value);
+    }
+    
+    /**
+     * Send the request
+     */
+    public PaymentResponse send() {
+        String data = getData();
+        return send(data);
+    }
+    
+    /**
+     * Get the associated response.
+     */
+    public PaymentResponse getResponse () {
+        if (response == null) {
+            throw new RuntimeException("You must call send() before accessing the Response!");
+        }
+        return response;
+    }
+}
---
+++ core/src/test/java/org/opencps/payment/RequestBaseTest.java
@@ -0,0 +1,368 @@
+/**
+* OpenCPS Payment is the open source Core Payment Connector software
+* Copyright (C) 2016-present OpenCPS community
+
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+package org.opencps.payment;
+
+import org.apache.commons.collections4.IterableMap;
+import org.apache.commons.collections4.map.HashedMap;
+import org.opencps.payment.api.PaymentItem;
+import org.opencps.payment.api.PaymentResponse;
+import org.opencps.payment.exception.InvalidRequestException;
+
+import org.opencps.payment.api.PaymentRequest;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Unit test for Request Base.
+ *
+ * @author Nguyen Van Nguyen <nguyennv@iwayvietnam.com>
+ */
+public class RequestBaseTest extends TestCase {
+
+    private RequestBase request;
+    
+    /**
+     * Create the test case
+     * 
+     * @param testName name of the test case
+     */
+    public RequestBaseTest(String testName) {
+        super(testName);
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() {
+        request = mock(RequestBase.class, CALLS_REAL_METHODS);
+        IterableMap<String, String> params = new HashedMap<String, String>();
+        request.initialize(params);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite(RequestBaseTest.class);
+    }
+
+    public void testInitializeWithParams() {
+        IterableMap<String, String> params = new HashedMap<String, String>();
+        params.put("amount", "1.23");
+        request.initialize(params);
+        assertEquals("1.23", request.getParameters().get("amount"));
+        try {
+            assertEquals("1.23", request.getAmount());
+        } catch (InvalidRequestException e) {
+            fail(e.getMessage());
+        }
+    }
+    
+    public void testInitializeAfterRequestSent() {
+        RequestBase request = mock(MockAbstractRequest.class, CALLS_REAL_METHODS);
+        request.send();
+        IterableMap<String, String> params = new HashedMap<String, String>();
+        try {
+            request.initialize(params);
+            fail("Missing exception");
+        }
+        catch (RuntimeException ex) {
+            assertEquals("Request cannot be modified after it has been sent!", ex.getMessage());
+        }
+    }
+    
+    public void testCard() {
+        CreditCard card = new CreditCard();
+        assertEquals(request, request.setCard(card));
+        assertEquals(card, request.getCard());
+    }
+    
+    public void testSetCardWithParams() {
+        IterableMap<String, String> params = new HashedMap<String, String>();
+        params.put("number", "1234");
+        assertEquals(request, request.setCard(params));
+        CreditCard card = request.getCard();
+        assertEquals("1234", card.getNumber());
+    }
+    
+    public void testToken() {
+        assertEquals(request, request.setToken("1234"));
+        assertEquals("1234", request.getToken());
+    }
+    
+    public void testCardReference() {
+        assertEquals(request, request.setCardReference("1234"));
+        assertEquals("1234", request.getCardReference());
+    }
+    
+    public void testAmount() throws InvalidRequestException {
+        assertEquals(request, request.setAmount("1.23"));
+        assertEquals("1.23", request.getAmount());
+
+        assertEquals(request, request.setAmount(1.23f));
+        assertEquals("1.23", request.getAmount());
+    }
+    
+    public void testAmountWithEmpty() throws InvalidRequestException {
+        String amount = null;
+        assertEquals(request, request.setAmount(amount));
+        assertNull(request.getAmount());
+    }
+    
+    public void testAmountZeroFloat() throws InvalidRequestException {
+        assertEquals(request, request.setAmount(0.00f));
+        assertEquals("0.0", request.getAmount());
+    }
+    
+    public void testAmountZeroString() throws InvalidRequestException {
+        assertEquals(request, request.setAmount("0.00"));
+        assertEquals("0.00", request.getAmount());
+    }
+    
+    public void testAmountZeroNotAllowed() {
+        MockAbstractRequest request = new MockAbstractRequest();
+        request.zeroAmountNotAllowed().setAmount("0.00");
+        try {
+            request.getAmount();
+            fail("Missing exception");
+        } catch (InvalidRequestException e) {
+            assertEquals("A zero amount is not allowed.", e.getMessage());
+        }
+    }
+    
+    public void testAmountPrecisionTooHigh() {
+        assertEquals(request, request.setAmount("123.005"));
+        try {
+            request.getAmount();
+            fail("Missing exception");
+        } catch (InvalidRequestException e) {
+            assertEquals("Amount precision is too high for currency.", e.getMessage());
+        }
+    }
+    
+    public void testGetAmountNoDecimals() throws InvalidRequestException {
+        assertEquals(request, request.setCurrency("VND"));
+        assertEquals(request, request.setAmount("1234"));
+        assertEquals("1234", request.getAmount());
+    }
+    
+    public void testGetAmountNoDecimalsRounding() {
+        assertEquals(request, request.setCurrency("VND"));
+        assertEquals(request, request.setAmount("123.4"));
+        try {
+            request.getAmount();
+            fail("Missing exception");
+        } catch (InvalidRequestException e) {
+            assertEquals("Amount precision is too high for currency.", e.getMessage());
+        }
+    }
+    
+    public void testAmountWithIntThrowsException() {
+        assertEquals(request, request.setCurrency("USD"));
+        assertEquals(request, request.setAmount("123"));
+        try {
+            request.getAmount();
+            fail("Missing exception");
+        } catch (InvalidRequestException e) {
+            assertEquals("Please specify amount as a float string, with decimal places (e.g. \'10.00\' to represent $10.00).", e.getMessage());
+        }
+    }
+    
+    public void testGetAmountInteger() throws InvalidRequestException {
+        assertEquals(request, request.setAmount("12.34"));
+        assertEquals(new Integer(1234), request.getAmountInteger());
+    }
+    
+    public void testGetAmountIntegerNoDecimals() throws InvalidRequestException {
+        assertEquals(request, request.setCurrency("VND"));
+        assertEquals(request, request.setAmount("1234"));
+        assertEquals(new Integer(1234), request.getAmountInteger());
+    }
+
+    public void testAmountNegativeStringThrowsException() {
+        assertEquals(request, request.setAmount("-123.00"));
+        try {
+            request.getAmount();
+            fail("Missing exception");
+        } catch (InvalidRequestException e) {
+            assertEquals("A negative amount is not allowed.", e.getMessage());
+        }
+    }
+    
+    public void testAmountNegativeFloatThrowsException() {
+        assertEquals(request, request.setAmount(-123.00f));
+        try {
+            request.getAmount();
+            fail("Missing exception");
+        } catch (InvalidRequestException e) {
+            assertEquals("A negative amount is not allowed.", e.getMessage());
+        }
+    }
+    
+    public void testCurrency() {
+        assertEquals(request, request.setCurrency("VND"));
+        assertEquals("VND", request.getCurrency());
+    }
+    
+    public void testCurrencyLowercase() {
+        assertEquals(request, request.setCurrency("vnd"));
+        assertEquals("VND", request.getCurrency());
+    }
+    
+    public void testCurrencyNumeric() {
+        assertEquals(request, request.setCurrency("VND"));
+        assertEquals("704", request.getCurrencyNumeric());
+    }
+    
+    public void testCurrencyDecimals() {
+        assertEquals(request, request.setCurrency("VND"));
+        assertEquals(new Integer(0), request.getCurrencyDecimalPlaces());
+    }
+    
+    public void testDescription() {
+        assertEquals(request, request.setDescription("OpenCps Payment Connector"));
+        assertEquals("OpenCps Payment Connector", request.getDescription());
+    }
+    
+    public void testTransactionId() {
+        assertEquals(request, request.setTransactionId("1234"));
+        assertEquals("1234", request.getTransactionId());
+    }
+    
+    public void testTransactionReference() {
+        assertEquals(request, request.setTransactionReference("abc"));
+        assertEquals("abc", request.getTransactionReference());
+    }
+    
+    public void testItems() {
+        List<PaymentItem> items = new ArrayList<PaymentItem>();
+        Item item = new Item();
+        item.setName("Floppy Disk");
+        items.add(item);
+        assertEquals(request, request.setItems(items));
+        assertEquals("Floppy Disk", request.getItems().get(0).getName());
+    }
+    
+    public void testClientIp() {
+        assertEquals(request, request.setClientIp("127.0.0.1"));
+        assertEquals("127.0.0.1", request.getClientIp());
+    }
+    
+    public void testReturnUrl() {
+        assertEquals(request, request.setReturnUrl("https://www.example.com/return"));
+        assertEquals("https://www.example.com/return", request.getReturnUrl());
+    }
+    
+    public void testCancelUrl() {
+        assertEquals(request, request.setCancelUrl("https://www.example.com/cancel"));
+        assertEquals("https://www.example.com/cancel", request.getCancelUrl());
+    }
+    
+    public void testNotifyUrl() {
+        assertEquals(request, request.setNotifyUrl("https://www.example.com/notify"));
+        assertEquals("https://www.example.com/notify", request.getNotifyUrl());
+    }
+    
+    public void testIssuer() {
+        assertEquals(request, request.setIssuer("OpenCps Bank"));
+        assertEquals("OpenCps Bank", request.getIssuer());
+    }
+    
+    public void testPaymentMethod() {
+        assertEquals(request, request.setPaymentMethod("credit"));
+        assertEquals("credit", request.getPaymentMethod());
+    }
+    
+    public void testTestMode() {
+        assertEquals(request, request.setTestMode(true));
+        assertTrue(request.getTestMode());
+    }
+    
+    public void testSetParameterAfterRequestSent() {
+        RequestBase request = new MockAbstractRequest();
+        request.send();
+        try {
+            request.setCurrency("VND");
+            fail("Missing exception");
+        }
+        catch (RuntimeException e) {
+            assertEquals("Request cannot be modified after it has been sent!", e.getMessage());
+        }
+    }
+    
+    public void testSend() {
+        String data = "request data";
+        PaymentResponse response = mock(PaymentResponse.class);
+        PaymentRequest request = mock(RequestBase.class, CALLS_REAL_METHODS);
+        when(request.getData()).thenReturn(data);
+        when(request.send(request.getData())).thenReturn(response);
+        assertNotNull(request.send());
+        assertSame(response, request.send());
+    }
+    
+    public void testGetResponseBeforeRequestSent() {
+        RequestBase request = new MockAbstractRequest();
+        try {
+            request.getResponse();
+            fail("Missing exception");
+        }
+        catch (RuntimeException e) {
+            assertEquals("You must call send() before accessing the Response!", e.getMessage());
+        }
+    }
+    
+    public void testGetResponseAfterRequestSent() {
+        RequestBase request = new MockAbstractRequest();
+        request.send();
+        assertTrue(request.getResponse() instanceof PaymentResponse);
+    }
+    
+    class MockAbstractRequest extends RequestBase {
+
+        public MockAbstractRequest() {
+            super(mock(ConnectorBase.class, CALLS_REAL_METHODS));
+        }
+
+        public MockAbstractRequest(ConnectorBase connector) {
+            super(connector);
+        }
+        
+        public MockAbstractRequest zeroAmountNotAllowed() {
+            zeroAmountAllowed = false;
+            return this;
+        }
+
+        @Override
+        public PaymentResponse send(String data) {
+            response = mock(PaymentResponse.class, CALLS_REAL_METHODS);
+            return response;
+        }
+
+        @Override
+        public String getData() {
+            return null;
+        }
+        
+    }
+}
+
